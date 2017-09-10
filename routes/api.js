eb3f8cadaf99d2e683881140a8e3e7874adf402d
Vishnu <pvishnuprasaath@gmail.com>
2017-09-11 00:49:37 +0530

    relative paths updated which works on local version

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,31 @@
 const express=require('express')
 const bodyParser = require('body-parser')
 const CryptoJS = require("crypto-js")
-const rsa=require('../helpers/rsafun')
+var crypto = require("crypto")
+var path = require("path")
+var fs = require("fs")
 const router=express.Router()
 
 
+var encryptStringWithRsaPublicKey = function(toEncrypt, relativeOrAbsolutePathToPublicKey) {
+    //var absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey)
+    var publicKey = fs.readFileSync(relativeOrAbsolutePathToPublicKey, "utf8")
+    var buffer = new Buffer(toEncrypt)
+    var encrypted = crypto.publicEncrypt(publicKey, buffer)
+    return encrypted.toString("base64")
+};
+
+var decryptStringWithRsaPrivateKey = function(toDecrypt, relativeOrAbsolutePathtoPrivateKey) {
+   // var absolutePath = path.resolve(relativeOrAbsolutePathtoPrivateKey)
+    var privateKey = fs.readFileSync(relativeOrAbsolutePathtoPrivateKey, "utf8")
+    var buffer = new Buffer(toDecrypt, "base64")
+    var decrypted = crypto.privateDecrypt(privateKey, buffer)
+    return decrypted.toString("utf8")
+};
+
+
 var admin = require("firebase-admin");
-var serviceAccount = require("../helpers/cert/serviceAccountKey.json");
+var serviceAccount = require("./cert/serviceAccountKey.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://bucketcrypt.firebaseio.com"
@@ -18,7 +37,7 @@ var dataRef = ref.child("secretdata");
 
 router.post('/rsa/enc',function(req,res){
     if (!req.body) return res.sendStatus(400)
-    var cipherText=rsa.encryptStringWithRsaPublicKey(req.body.text,'../helpers/cert/publicKey.PEM')
+    var cipherText=encryptStringWithRsaPublicKey(req.body.text,'./routes/cert/publicKey.pem')
     console.log(cipherText.toString())
     dataRef.push({
         text: req.body.text,
@@ -38,7 +57,7 @@ router.get('/rsa/dec',function(req,res){
         var data = snapshot.val();
         console.log(data);
         for(el in data){
-            data[el].cipher=rsa.decryptStringWithRsaPrivateKey(data[el].cipher,'../helpers/cert/privateKey.PEM')
+            data[el].cipher=decryptStringWithRsaPrivateKey(data[el].cipher,'./routes/cert/privateKey.pem')
            //result[key].push(data[el]); 
         }
         //console.log(JSON.stringify(result));
@@ -49,7 +68,7 @@ router.get('/rsa/dec',function(req,res){
 
 router.post('/rsa/dec',function(req,res){
     if (!req.body) return res.sendStatus(400)
-    var plainText=rsa.decryptStringWithRsaPrivateKey(req.body.cipher,'../helpers/cert/privateKey.PEM')
+    var plainText=decryptStringWithRsaPrivateKey(req.body.cipher,'./routes/cert/privateKey.pem')
     console.log(plainText)    
     res.send({
         Cipher: req.body.cipher,
