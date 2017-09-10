cc5da14e2bd106bd50e27972483b7f29a8677ccf
Vishnu <pvishnuprasaath@gmail.com>
2017-09-10 22:43:47 +0530

    Initial Push

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const CryptoJS = require("crypto-js")
+const routes=require('./routes/api')
+const cors = require('cors')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use('/api',routes)
+app.use(express.static(path.join(__dirname,'public')))
+
+// app.get('/', function (req, res) {
+//     res.send(' Welcome to BucketCrypt '+ ciphertext) 
+// })
+
+app.get('*',function(req,res){
+  res.redirect('/')
+})
+
+app.listen(process.env.PORT||8080, function () {
+  console.log('Server started at port 8080')
+})
